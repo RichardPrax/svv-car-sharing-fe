6fb8dc84b5d35745141b55cf7e4f108f72b3213e
Richard <richardprax2003@gmail.com>
2024-10-14 18:18:17 +0200

    feat: working

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../../student.service';
 import { Student } from '../../student';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -17,6 +18,9 @@ export class HomeComponent implements OnInit{
   students: Student[] = [];
 
   ngOnInit(): void {
-      this.studentService.getAllStudents();
+      this.studentService.getAllStudents().subscribe(data => {
+        console.log(data);
+        this.students = data}
+      );
   }
 }
